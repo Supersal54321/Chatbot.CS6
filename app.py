3fa5a9b1a1001e46e510bf511344d96fac1668f0
Supersal54321 <123377585+Supersal54321@users.noreply.github.com>
2023-03-03 16:12:10 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from chatbot import predict_class, get_response, intents
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "a_secretive_key_123456789"
+
+response_list = []
+
+@app.route("/")
+def home():
+    global response_list
+    response_list.clear()
+    return render_template("index.html")
+
+@app.route("/chatbot", methods=["GET", "POST"])
+def chatbot():
+    global response_list
+    if request.method == "POST":
+        message = request.form['message']
+        message = message.lower()
+        ints = predict_class(message)
+        res = get_response(ints, intents)
+        response_list.append(message)
+        response_list.append(res)
+        return render_template("chatbot.html", message=message, response_list=response_list)
+    return render_template("chatbot.html", message="", response_list=response_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
